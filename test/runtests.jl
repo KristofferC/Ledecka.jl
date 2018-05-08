30a5b9f3217fc2493dfa40cc2aa3dbf7a1d862dd
Polytomous! <polytomous@gmail.com>
2018-05-07 17:48:49 -0700

    Add test set groupings to the runtests.jl file

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,23 @@
+using Test 
+
+@testset "Ledecka.jl tests" begin
+
+@testset "Core Tests" begin 
 include("CoreTests.jl")
+end
+
+@testset "Default Arbitrary Implementation Tests" begin
 include("arbitrary/number_tests.jl")
 include("arbitrary/sequence_tests.jl")
 include("arbitrary/unicode_tests.jl")
 include("arbitrary/dictionary_tests.jl") 
 include("arbitrary/struct_tests.jl") 
 include("arbitrary/union_tests.jl")
+end
 
-
+@testset "Example Tests" begin
 include("PaperExample.jl")
 include("CheckersExample.jl")
+end
+end
 
